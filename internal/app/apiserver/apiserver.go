c81eb33136d3c35aea24790e275476e14f6801d0
Orest <orest-2@yandex.ua>
2020-02-27 14:33:36 +0200

    Fix

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -105,7 +105,9 @@ func CORS(next http.Handler) http.Handler {
 
 func (s *APIServer) configureRouter() {
 	s.router.Use(CORS)
-	s.router.HandleFunc("/api/calc", s.handleCalc()).Methods(http.MethodPost, http.MethodOptions)
+
+	api := s.router.PathPrefix("/api/").Subrouter()
+	api.HandleFunc("calc", s.handleCalc()).Methods(http.MethodPost, http.MethodOptions)
 
 	s.router.PathPrefix("/").Handler(s)
 }
